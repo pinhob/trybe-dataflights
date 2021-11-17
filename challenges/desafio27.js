16c0918d9e8dc355aa020e357bf4c3030aef13be
Bruno Pinho <brunopinho@outlook.com>
2021-11-17 19:19:53 -0300

    feito requisito 27

---
+++ challenges/desafio27.js
@@ -0,0 +1,15 @@
+db.voos.find({ $and: 
+  [{ "empresa.nome": "PASSAREDO" }, 
+  { natureza: "Doméstica" }] }).count();
+
+db.resumoVoos.insertOne(
+  {
+    "empresa": "PASSAREDO",
+    "totalVoosDomesticos": 4187
+  },
+);
+
+db.resumoVoos.findOne(
+  { empresa: "PASSAREDO" },
+  { _id: 0, empresa: 1, totalVoosDomesticos: 1 },
+);
